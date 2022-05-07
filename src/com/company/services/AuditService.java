98c1e498edf4233af0e5e7f56cf68a4a3e19bd5f
AlexandraBoghiu <aboghiu2001@gmail.com>
2022-05-07 12:45:50 +0300

    fix audit service

--- src/com/company/services/AuditService.java
+++ src/com/company/services/AuditService.java
@@ -9,35 +9,22 @@ import java.util.Date;
 
 public class AuditService {
     private static AuditService instance = null;
+
     private AuditService() {
 
     }
+
     public static AuditService getInstance() throws IOException {
         if (instance != null) {
             return instance;
         }
         instance = new AuditService();
         File yourFile = new File("src\\com\\company\\resources\\audit.csv");
-        yourFile.createNewFile();
-        try (FileWriter writer = new FileWriter("src\\com\\company\\resources\\audit.csv", true)) {
-            writer.append("Action");
-            writer.append(",");
-            writer.append("Time");
-            writer.append(System.lineSeparator());
-            writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            System.out.println("Something didn't work while writing to Audit.");
-        }
-        return instance;
-    }
-    public void writeToAudit(String write) throws IOException {
-
+        if (yourFile.createNewFile()) {
             try (FileWriter writer = new FileWriter("src\\com\\company\\resources\\audit.csv", true)) {
-                writer.append(write);
+                writer.append("Action");
                 writer.append(",");
-                writer.append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")).toString());
+                writer.append("Time");
                 writer.append(System.lineSeparator());
                 writer.flush();
             } catch (IOException e) {
@@ -45,6 +32,23 @@ public class AuditService {
             } catch (Exception e) {
                 System.out.println("Something didn't work while writing to Audit.");
             }
+        }
+        return instance;
+    }
+
+    public void writeToAudit(String write) throws IOException {
+
+        try (FileWriter writer = new FileWriter("src\\com\\company\\resources\\audit.csv", true)) {
+            writer.append(write);
+            writer.append(",");
+            writer.append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")).toString());
+            writer.append(System.lineSeparator());
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Something didn't work while writing to Audit.");
+        }
 
 
     }
